3466b0521b7c8ac2d0c51d88e8bc31c83ef7ce04
Kagemaru1 <kagemaru888888@gmail.com>
2022-02-28 13:30:10 +0200

    Tasks

--- SoftUni/Basics/JS-Programming-Basics-Online-Exam-18-And-19-July-2020/01.agencyProfit.js
+++ SoftUni/Basics/JS-Programming-Basics-Online-Exam-18-And-19-July-2020/01.agencyProfit.js
@@ -1,4 +1,16 @@
 function agencyProfit(input) {
-    
+	let agency = input[0];
+	let adultTickets = Number(input[1]);
+	let childrenTickets = Number(input[2]);
+	let adultTicketsPrice = Number(input[3]);
+	let tax = Number(input[4]);
+
+	let childrenTicketsPrice = (adultTicketsPrice * 0.3 + tax) * childrenTickets;
+	adultTicketsPrice = (adultTicketsPrice + tax) * adultTickets;
+
+	let agencyProfit = (childrenTicketsPrice + adultTicketsPrice) * 0.2;
+
+	console.log(
+		`The profit of your agency from ${agency} tickets is ${agencyProfit.toFixed(2)} lv.`);
 }
 agencyProfit(["WizzAir", "15", "5", "120", "40"]);
