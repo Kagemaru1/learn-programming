3f1e1c2c6c6e211cf77c937fc54b18a0f6036d37
Kagemaru1 <kagemaru888888@gmail.com>
2022-03-03 15:41:39 +0200

    Task

--- SoftUni/Basics/JS-Conditional-Statements-Exercise/03.time+15Minutes.js
+++ SoftUni/Basics/JS-Conditional-Statements-Exercise/03.time+15Minutes.js
@@ -1,12 +1,21 @@
 function timePlus15Minutes(input) {
-    let hour = Number(input[0]);
-    let minutes = Number(input[1]);
+	let hour = Number(input[0]);
+	let minutes = Number(input[1]);
 
-    minutes += 15;
+	minutes += 15;
 
-    if (minutes >= 60) {
-        hour += 1;
-    }
+	if (minutes >= 60) {
+		minutes -= 60;
+		hour += 1;
+	}
+	if (hour > 23) {
+		hour = 0;
+	}
+	if (minutes > 9) {
+		console.log(`${hour}:${minutes}`);
+	} else {
+		console.log(`${hour}:0${minutes}`);
+	}
 }
 timePlus15Minutes(["1", "46"]);
 timePlus15Minutes(["0", "01"]);
